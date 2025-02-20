25982539b88fda6cddeb700d208eafbb13a6901b
EvgeniyKrastev <65820929+EvgeniyKrastev@users.noreply.github.com>
2025-02-20 12:12:04 +0200

    first commit

---
+++ votingapp/src/main/java/com/voting/votingapp/model/OptionVote.java
@@ -0,0 +1,28 @@
+package com.voting.votingapp.model;
+
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public class OptionVote {
+    private String optionText;
+    private Long voteCount = 0L;
+
+    public OptionVote() {
+    }
+
+    public String getOptionText() {
+        return optionText;
+    }
+
+    public void setOptionText(String optionText) {
+        this.optionText = optionText;
+    }
+
+    public Long getVoteCount() {
+        return voteCount;
+    }
+
+    public void setVoteCount(Long voteCount) {
+        this.voteCount = voteCount;
+    }
+}
